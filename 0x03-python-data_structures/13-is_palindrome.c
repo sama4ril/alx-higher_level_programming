4532a77a2596337e26bda0ade9b8237ffca3c009
Samaila Bitrus Mailabari <sama4ril@gmail.com>
2022-12-07 14:16:04 -0800

    Python - Data Structures

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ *is_palindrome - checks for list is palindrome
+ * @head: head of linked list
+ *Return: boolean
+ *     */
+
+
+
+int is_palindrome(listint_t **head)
+{
+	int len = 0, i = 0;
+	listint_t *tmp;
+	int Ns[10000];
+	tmp = *head;
+	if ((*head) == NULL)
+		return (1);
+	while (tmp != NULL)
+	{
+	le
+	tmp = tmp->next;
+	}
+	if (len == 1)
+	return (1);
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		Ns[i] = tmp->n;
+		tmp = tmp->next;																	i++;
+																				}
+	for (i = 0; i <= len / 2
+			{
+			if (Ns[i] != Ns[len - i - 1])																{
+		return (0);
+		}								
+		}									
+		return (1);
+}
